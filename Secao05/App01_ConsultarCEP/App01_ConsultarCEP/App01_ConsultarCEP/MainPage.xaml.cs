ce338d75e2c5836afc0967cb5054f16c70054896
Thiago Barcelos Mourao <thiagobarcelos1@hotmail.com>
2018-04-18 23:59:16 -0300

    App01 - Concluido

---
+++ Secao05/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App01_ConsultarCEP.Servico;
+using App01_ConsultarCEP.Servico.Modelo;
+
+namespace App01_ConsultarCEP
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+			Botao.Clicked += BuscarCep;
+		}
+
+		private void BuscarCep(object sender, EventArgs args)
+		{
+			string cep = txtCep.Text?.Trim();
+
+			lblResultado.Text = "";
+
+			if (isValidCEP(cep))
+			{
+				try
+				{
+					Endereco end = ViaCEPServico.BuscarEnderecoViaCep(cep);
+
+					lblResultado.Text = end.ToString();
+				}
+				catch (Exception ex)
+				{
+					DisplayAlert("ERRO CRITICO", ex.Message, "OK");
+				}
+			}
+
+		}
+
+		private bool isValidCEP(string cep)
+		{
+			if (string.IsNullOrEmpty(cep))
+				return false;
+
+			if (cep.Length != 8)
+			{
+				DisplayAlert("ERRO", "Cep deve conter 8 caracteres.", "OK");
+				return false;
+			}
+
+			int aux = 0;
+			if (!int.TryParse(cep, out aux))
+			{
+				DisplayAlert("ERRO", "Cep deve possuir apenas numeros.", "OK");
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
---
+++ Secao05/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/Servico/ViaCEPServico.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App01_ConsultarCEP.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace App01_ConsultarCEP.Servico
+{
+	public class ViaCEPServico
+	{
+		private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+		public static Endereco BuscarEnderecoViaCep(string cep)
+		{
+			string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+
+			WebClient wc = new WebClient();
+			string conteudo = wc.DownloadString(NovoEnderecoURL);
+
+			return JsonConvert.DeserializeObject<Endereco>(conteudo);
+		}
+	}
+}
